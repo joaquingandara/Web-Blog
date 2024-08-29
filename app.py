f8ea475267035f702138735761391507637ae62b
joaquingandara <gandara.joaquin65@gmail.com>
2024-08-28 22:34:33 -0300

    Display DB content on their pages

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row #hace que las filas devueltas por las consultas SQL se comporten como diccionarios
     return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 app = Flask(__name__) #Create app instance
 
@@ -18,3 +28,9 @@ def index():
     conn.close()
     return render_template('index.html',posts=posts) #Render_template helps you render html file located in /templates.
     #posts = posts returning post when index called
+
+# IF an user want to access .../1 then the decorator will call post with 1 as argument.
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
